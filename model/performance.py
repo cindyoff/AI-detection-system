0c978f439a89223c8373e23c2a4dbba6987c1978
陈 (Cindy) <79788527+cindyoff@users.noreply.github.com>
2025-04-05 19:22:32 +0200

    Update performance.py

--- model/performance.py
+++ model/performance.py
@@ -1,29 +1,29 @@
-# Prédiction et évaluation sur la partie restante de la base test
+# prediction and assessment of the test dataset left
 y_pred = clf.predict(X_test_full)
 print('Classification Report:')
 print(classification_report(y_test, y_pred))
 
-# Matrice de confusion
+# confusion matrix
 cm = confusion_matrix(y_test, y_pred)
 plt.figure(figsize=(6, 4))
 sns.heatmap(cm, annot=True, fmt='d', cmap='Blues')
-plt.xlabel('Prédictions')
-plt.ylabel('Vraies valeurs')
-plt.title('Matrice de Confusion')
+plt.xlabel('Predictions')
+plt.ylabel('Real values')
+plt.title('Confusion matrix')
 plt.show()
 
-# Importance des features manuelles
+# variables' feature importance
 feature_importance = clf.feature_importances_[-X_train_manual.shape[1]:]
 feature_names = X_train.drop(columns=['article']).columns
 
 plt.figure(figsize=(10, 6))
 sns.barplot(x=feature_importance, y=feature_names)
-plt.title("Importance des Features Manuelles")
+plt.title("Feature importance of each variable")
 plt.xlabel("Importance")
 plt.ylabel("Feature")
 plt.show()
 
-# courbe ROC et AUC
+# ROC curve and AUC value
 y_proba = clf.predict_proba(X_test_full)[:, 1]
 fpr, tpr, thresholds = roc_curve(y_test, y_proba)
 roc_auc = auc(fpr, tpr)
@@ -33,10 +33,10 @@ plt.plot(fpr, tpr, color='darkorange', lw=2, label='Courbe ROC (AUC = %0.2f)' % 
 plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
 plt.xlim([0.0, 1.0])
 plt.ylim([0.0, 1.05])
-plt.xlabel('Taux de Faux Positifs')
-plt.ylabel('Taux de Vrais Positifs')
-plt.title('Courbe ROC - Modèle XGBoost')
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.title('ROC curve - XGBoost model')
 plt.legend(loc="lower right")
 plt.show()
 
-print(f"Aire sous la courbe ROC (AUC): {roc_auc:.4f}")
+print(f"Area under the ROC curve (AUC) : {roc_auc:.4f}")
