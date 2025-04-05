c07b751a14c27dfebabbe7f1a10817e93b920dfa
陈 (Cindy) <79788527+cindyoff@users.noreply.github.com>
2025-04-05 19:02:57 +0200

    Update statistical tests.py

--- descriptive stats/statistical tests.py
+++ descriptive stats/statistical tests.py
@@ -2,7 +2,7 @@ char_pval = ttest_ind(test[test['class'] == 0]['char_length'], test[test['class'
 word_pval = ttest_ind(test[test['class'] == 0]['word_count'], test[test['class'] == 1]['word_count'], equal_var=False).pvalue
 
 print(f"\nT-test length (characters) — p-value : {char_pval:.4e}")
-print("Significant difference" if char_pval < 0.05 else "No significant difference")
+print("Significant difference" if char_pval < 0.05 else "Non-significant difference")
 
 print(f"\nT-test number of words — p-value : {word_pval:.4e}")
-print("Significative difference" if word_pval < 0.05 else "No significant difference")
+print("Significant difference" if word_pval < 0.05 else "Non-significant difference")
